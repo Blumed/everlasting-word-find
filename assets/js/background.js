4c269dad45f41da012a510a77af51142f55df147
Cullan Luther <cullan.luther@wheniwork.com>
2020-12-29 22:28:22 -0600

    initial commit

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,28 +1,10 @@
-var background = {
-    style: "",
-    init: function() {
-
-        chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-                //listen to any messages, and route them to functions
-                if(request.fn in background) {
-                    background[request.fn](request, sender, sendResponse);
-                }
-            });
-    },
-
-    // setSelections: function(request, sender, sendResponse) {
-    //     // console.log('setting selectors', request.selector);
-    //      console.log('setting style', request.style);
-    //     // console.log('setting matched', request.matched);
-    //     this.style = request.style;
-    // },
-
-    getSelector: function(request, sender, sendResponse) {
-        // console.log('getting selector', this.selector);
-         console.log('getting style', this.style);
-        // console.log('getting matched', this.matched);
-        sendResponse({style: this.style });
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+       localStorage["total_elements"] = request.total_elements;
     }
-}
-
-background.init();
+);
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { type: "popup-modal" });
+    });
+});
--- assets/js/contentScript.js
+++ assets/js/contentScript.js
@@ -1,47 +1,100 @@
-'use strict';
+const getRenderedWords = document.body.innerText
+    //convert to lowercase
+    .toLowerCase()
+    // remove special characters
+    .replace(/\'s+/, '')
+    .replace(/[0-9`~!@#$%^&*()_|+\-\–=?;:'",.<>\{\}\[\]\\\/]/gi, " ")
+    .replace(/`${}`/gi, '');
 
-// chrome.runtime.sendMessage({ fn: "getSelector" } , function(response) {
-// 	//console.log(response.selector, response.style);
-//     $('head').append($('<style type="text/css">' + response.style + '</style>'));
 
-// });
+const unnecessaryWords = ['what', 'where', 'from', 'want', 'use', 'using', 'used', 'share', 'sharing', 'ask', 'asking', 'asked', 'that', 'this', 'have', 'should', 'anything', 'here', 'with', 'will', "they", "them", "their", "theirs", "themself", "herself", "himself", 'you', 'your', 'would', 'do', 'does', 'such', 'follow', 'before', 'after', 'also', 'just', 'need', 'needing', 'needs', 'did', 'didnt', 'show', 'showing', 'hide', 'find', 'finding', 'public', 'policy', 'see', 'seeing', 'thing', 'things', 'while', 'about', 'ever', 'more', 'for', 'some', 'sitemap', 'home', 'between', 'privacy', 'mean'];
 
-      // function saveChanges() {
-      //   // Get a value saved in a form.
-      //   var theValue = textarea.value;
-      //   // Check that there's some code there.
-      //   if (!theValue) {
-      //     message('Error: No value specified');
-      //     return;
-      //   }
-      //   // Save it using the Chrome extension storage API.
-      //   chrome.storage.sync.set({'value': theValue}, function() {
-      //     // Notify that we saved.
-      //     message('Settings saved');
-      //   });
-      // }
+function escape(s) {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
 
-      //   chrome.storage.onChanged.addListener(function(changes, namespace) {
-      //   for (key in changes) {
-      //     var storageChange = changes[key];
-      //     console.log('Storage key "%s" in namespace "%s" changed. ' +
-      //                 'Old value was "%s", new value is "%s".',
-      //                 key,
-      //                 namespace,
-      //                 storageChange.oldValue,
-      //                 storageChange.newValue);
-      //   }
-      // });
-// var customStyles = document.createElement('style'); 
-// customStyles.appendChild(document.createTextNode(
-//    'body { background-color: ' + localStorage.getItem('background-color') + '}'
-// ));
-// document.documentElement.insertBefore(customStyles); 
-// function injectStyles(file, node) {
-//   var th = document.body;
-//   var s = document.createElement('link');
-//   s.rel = 'stylesheet'
-//   s.setAttribute('href', file);
-//   th.appendChild(s);
-// }
-// injectStyles(chrome.extension.getURL('assets/css/style.css'), 'body');
+function removeWords(arr, str) {
+    let escapedArr = arr.map(v => escape(v))
+    let regex = new RegExp("(?:^|\\s)" + escapedArr.join('|') + "(?!\\S)", "gi")
+    return str.replace(regex, '');
+
+}
+
+function removeDuplicates(data) {
+    return [...new Set(data)]
+};
+
+const arr = (str, word) => {
+    str = str
+        .split(" ")
+        .filter((str) => {
+            word = str.match(/(\w+)/);
+            return word && word[0].length > 3;
+        })
+        .filter((str) => {
+            word = str.match(/(\w+)/);
+            return word && word[0].length < 15;
+        })
+        .join(" ");
+    return str.match(/\b(\w+)\b/g);
+};
+
+
+let data = arr(removeWords(unnecessaryWords, getRenderedWords));
+
+const results = removeDuplicates(data)
+// console.log('results', results);
+
+
+
+let shuffled = results.sort(function() {
+    return .5 - Math.random()
+});
+let selected = (total) => shuffled.slice(0, total);
+
+const stats = {
+    basicClean: data.length,
+    removeDuplicates: results.length,
+    random20: selected(8)
+};
+
+// console.table(stats);
+// console.log("get results: ", results);
+
+chrome.runtime.sendMessage({
+    total_elements: results
+});
+
+chrome.runtime.onMessage.addListener((request) => {
+    if (request.type === 'popup-modal') {
+        showModal();
+    }
+})
+const showModal = () => {
+    const modal = document.createElement("dialog");
+    modal.setAttribute(
+        "style", `
+      width: 1000px;
+      height: 800px;
+      border: none;
+      top:0;
+      bottom:0;
+      border-radius:20px;
+      background-color:white;
+      position: fixed; box-shadow: 0px 12px 48px rgba(29, 5, 64, 0.32);
+      `
+    );
+    modal.innerHTML = `<iframe id="popup-content"; style="height:100%;width: 100%;"></iframe>
+      <div style="position:absolute; top:0px; left:0px;">
+      <button style="padding: 2px 15px; font-size: 20px; border: none; border-radius: 20px; border: 1px solid black;">x</button>
+      </div>`;
+    document.body.appendChild(modal);
+    const dialog = document.querySelector("dialog");
+    dialog.showModal();
+    const iframe = document.getElementById("popup-content");
+    iframe.src = chrome.extension.getURL("index.html");
+    iframe.frameBorder = 0;
+    dialog.querySelector("button").addEventListener("click", () => {
+        dialog.close();
+    });
+}
